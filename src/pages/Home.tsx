bb2e7cdfee378668e99ce23e29f25943e0501c5d
vitor7001 <vitorgomes7001@gmail.com>
2023-05-25 22:03:12 -0300

    feat: listando usuários na home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { sessionManager } from '../services/session.repository';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { listUsers } from '../services/user/user.list.service';
 
 export default function Home() {
   
-  const navigation = useNavigation();
+  const navigation = useNavigation<any>();
   
+  const [users, setUsers] = React.useState<any[]>([])
+
   React.useEffect(() => {
     navigation.setOptions({
       headerLeft: () => <Button title='Sair' onPress={() => navigation.goBack()}/>,
       headerRight: () => <Button title='Cadastrar' onPress={() => navigation.navigate('UserRegister')}/>
   
+    })
+
+    listUsers.listUser().then(data =>{
+      if(data.errorMessage){
+        Alert.alert('Erro ao listar usuários: ', data.errorMessage)
+      }else{
+        console.log("DATA")
+        console.log(data)
+        setUsers(data)
+      }
     })
   }, [])
 
@@ -20,14 +32,27 @@ export default function Home() {
   
   return (
     <View style={styles.container}>
-      <Text>Showw</Text>
       <StatusBar style="auto" />
-      <Text>{}</Text>
+      <Text>Listagem de usuários</Text>
+      <FlatList 
+      data={users}
+      renderItem={({item}) =>{
+        return (
+          <View style={styles.itemView}>
+          <Text >{item.id} - {item.name} - {item.username}</Text>
+          </View>
+        )
+      }}
+      
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  itemView:{
+    padding: 10
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
--- src/services/user/user.list.service.ts
+++ src/services/user/user.list.service.ts
@@ -4,7 +4,7 @@ class ListUsers{
 
     private readonly url = 'http://192.168.0.113:3000'
 
-    public async updateUser(){
+    public async listUser(){
         const user = await sessionManager.getLoggedUser()
 
         if(!user) throw new Error("Não há usuário no local storage para ser utilizado")
@@ -17,9 +17,15 @@ class ListUsers{
             }
         })
 
-        const userCreated = await res.json() 
 
-        return userCreated
+        if(res.status === 200){
+            const userCreated = await res.json() 
+
+            return userCreated 
+        }
+
+        const error = await res.json()
+        return {errorMessage: error.message}
     }
 
 }
