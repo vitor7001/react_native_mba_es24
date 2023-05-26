26f24233af54080a52540ce44061629511f77849
vitor7001 <vitorgomes7001@gmail.com>
2023-05-25 21:36:43 -0300

    feat: delete user service

---
+++ src/services/user/user.delete.service.ts
@@ -0,0 +1,28 @@
+import { sessionManager } from "../session.repository";
+
+class DeleteUser{
+
+    private readonly url = 'http://192.168.0.113:3000'
+
+    public async deleteUser(id: number){
+        const user = await sessionManager.getLoggedUser()
+
+        if(!user) throw new Error("Não há usuário no local storage para ser utilizado")
+
+        const res = await fetch(this.url + '/users/'  + id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.token}`
+            }
+        })
+
+        const userCreated = await res.json() 
+
+        return userCreated
+    }
+
+}
+
+
+export const deleteUser = new DeleteUser();
